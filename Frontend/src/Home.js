7b4bca546c4395b249cd90a89a214771bdd865e9
niemeyar.git <niemeyar@mail.uc.edu>
2025-02-17 14:27:50 -0500

    added outline for service subheading text

--- Frontend/src/Home.js
+++ Frontend/src/Home.js
@@ -11,9 +11,10 @@ const Home = () => {
             <header className="App-header">
                 <div>
                     <h1>Welcome to the CrisisConnect Dashboard</h1>
-                    <h2 id='banner'></h2> 
+                    <h3 id='banner'></h3> 
 
                     <div className="Chart-grid">
+                        <section>
                         <div className="Chart">
                         <p className="service">Service A</p>
                         {/*this is the code to output the graph */}
@@ -21,6 +22,8 @@ const Home = () => {
                             <Line type="monotone" dataKey="pv" stroke="#F97316" strokeWidth={2} dot={false}/>
                         </LineChart>
                         </div>
+                        <h5>sample text</h5>
+                        </section>
 
                         <div className="Chart">
                         <p className="service">Service B</p>
--- Frontend/src/Settings.js
+++ Frontend/src/Settings.js
@@ -5,9 +5,16 @@ const Settings = () => {
         <div className="App" >
             <header className="App-header">
                 <h1>Settings</h1>
-                <label>Set Banner Notification Text</label>
-                <textarea rows="4" columns="50"></textarea>
-                <br></br><br></br>
+                <label>Set banner notification text</label>
+                <textarea rows="4" columns="50"/>
+                <br/>
+                <input type="checkbox" id="myCheckbox" value="option1"/>
+                <label for="myCheckbox">Send out an email notification about this update</label>
+                <button>Update</button>
+                <br/>
+                <label>Set service a subtext</label>
+                <textarea rows="4" columns="50"/>
+                <br/>
                 <button>Update</button>
             </header>
         </div>
